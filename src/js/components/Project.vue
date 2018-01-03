2d862f5c2b94c233311ff5342c03bb49281d1fdb
Jeff Glenn <jeff@aviddesign.com>
2018-01-03 07:40:17 -0500

    Only render link if project URL not empty

--- src/js/components/Project.vue
+++ src/js/components/Project.vue
@@ -1,8 +1,10 @@
 <template>
         <div class="cell medium-6 project">
             <a class="project__image" data-featherlight="#project-lightbox" target="_blank" v-on:click.prevent="projectSelected"><img class="project-image" :src="projectData.img"></a>
-            <h3 class="project-title">{{projectData.name}}</h3>
-            <a v-if="projectData.url" target="_blank" class="project-url" :href="projectData.url">{{projectData.url}}</a>
+            <h3 class="project-title">
+                <a v-if="projectData.url" target="_blank" class="project-url" :href="projectData.url">{{projectData.name}}</a>
+                <span v-else>{{projectData.name}}</span>
+            </h3>
             <div class="project-body">
                 <div class="project-body__content" v-html="projectData.body"></div>
                 <ul v-if="projectData.notes && projectData.notes.length > 0" class="notes">
